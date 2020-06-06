a896df551cb1531d971dd28bce5cb82167d2de84
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-06 10:49:52 -0300

    Clean initial strutucture

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>LET'S GO NEXT LEVEL WEEK!!!</h1>
-      </header>
-    </div>
+    <h1>LET'S GO NEXT LEVEL WEEK!!!</h1>
   );
 }
 

30ba5fcdb2a79fd04c9b0df962fff41f7b0b6cf4
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-06 14:38:38 -0300

    Bomba

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './Pages/Home/Home';
+
+// JSX: Sintaxe de XML dentro do JavaScript
 
 function App() {
-  return (
-    <h1>LET'S GO NEXT LEVEL WEEK!!!</h1>
-  );
+  return <Home />;
 }
 
 export default App;

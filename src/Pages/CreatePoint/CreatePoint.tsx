9caf1c783fbe6399b11a53d003eb9d960880575d
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-06 19:49:42 -0300

    Bomba

--- src/Pages/CreatePoint/CreatePoint.tsx
+++ src/Pages/CreatePoint/CreatePoint.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import api from '../../services/api';
+import axios from 'axios';
 
 import './CreatePoint.css';
 
 import logo from '../../assets/logo.svg';
 import Mapa from './Map/Map';
 
+// Sempe que criamos um estado,
+//   precisamos informar manualmente os tipos
+
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+interface IBGEUFapiResponse {
+  sigla: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = React.useState<Item[]>([]);
+  const [ufs, setUfs] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    api.get('items').then((response) => {
+      setItems(response.data);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    axios
+      .get<IBGEUFapiResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+      .then((response) => {
+        const ufLetters = response.data.map(({ sigla }) => sigla);
+        setUfs(ufLetters);
+      });
+  }, []);
+
   return (
     <div id="page-create-point">
       <header>
@@ -56,9 +89,22 @@ const CreatePoint = () => {
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
               <select name="uf" id="uf">
-                <option value="0">Selecione uma cidade</option>
+                <option value="0">Selecione uma UF</option>
+                {ufs.map((uf) => (
+                  <option key={uf} value={uf}>
+                    {uf}
+                  </option>
+                ))}
               </select>
             </div>
+            {/* CIDADE!!! */}
+            {/* <div className="field">
+              <label htmlFor="uf">Cidade</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+                {}
+              </select>
+            </div> */}
           </div>
         </fieldset>
 
@@ -69,30 +115,12 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-              <span>Óleo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-              <span>Óleo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-              <span>Óleo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-              <span>Óleo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-              <span>Óleo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-              <span>Óleo de cozinha</span>
-            </li>
+            {items.map(({ id, title, image_url }) => (
+              <li key={id}>
+                <img src={image_url} alt={title} />
+                <span>{title}</span>
+              </li>
+            ))}
           </ul>
         </fieldset>
 
